c82891b60657b37c7f37cd3d6590d78df29f4450
Mohit Singh <mohittjee@gmail.com>
2025-09-20 08:22:50 +0530

    fix: deployment

--- src/components/features/index.ts
+++ src/components/features/index.ts
@@ -2,4 +2,3 @@
 export { BadgeDisplay } from './badge-display'
 export { ContributionGraph } from './contribution-graph'
 export { DiscordProfileCard, ProfileCardTrigger } from './discord-profile-card'
-export { default as HeroCard } from './hero-card'
