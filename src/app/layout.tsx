a96de9f78fd3979fa7a81461a052f1351a2d39ad
Mohit Singh <mohittjee@gmail.com>
2025-10-08 02:23:31 +0530

    add: meta data preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,37 @@ import { Suspense } from "react"
 import "../../styles/globals.css"
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://dev-quest-swart.vercel.app"),
   title: "DevQuest - Where Developers Compete, Contribute and Hunt Bounties",
   description:
     "Make open-source contributions, discover bounty-paying issue/repositories and rank among developers across the globe.",
-  generator: "v0.app",
+  generator: "dev-quest-swart.vercel.app",
+  openGraph: {
+    title: "DevQuest - Where Developers Compete, Contribute and Hunt Bounties",
+    description:
+      "Make open-source contributions, discover bounty-paying issues and rank among developers across the globe.",
+    url: "https://dev-quest-swart.vercel.app",
+    siteName: "DevQuest",
+    images: [
+      {
+        url: "/preview.png",
+        width: 1200,
+        height: 630,
+        alt: "DevQuest Preview Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "DevQuest - Where Developers Compete, Contribute and Hunt Bounties",
+    description:
+      "Join DevQuest: contribute, compete, and earn bounties with developers worldwide.",
+    images: ["/preview.png"],
+    creator: "@CalmNrd",
+  },
 }
 
 export default function RootLayout({
